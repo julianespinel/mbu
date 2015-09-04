e9aa7b80c425a440bc119c9c3ed91f6541871b4b
Julian Espinel <julianespinel@gmail.com>
2015-09-03 22:38:55 -0500

    Add functional product in super alpha state.

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/disk"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.String(200, "pong");
+	})
+
+	router.GET("/cpu", func(context *gin.Context) {
+		perCpu := true;
+		cpuPercentArray, _ := cpu.CPUPercent(time.Second, perCpu);
+		fmt.Println(cpuPercentArray);
+		context.String(200, "cpuUsage");
+	})
+
+	router.GET("/memory", func(context *gin.Context) {
+		vm, _ := mem.VirtualMemory();
+		fmt.Println(vm);
+		context.String(200, "memoryUsage");
+	})
+
+	router.GET("/disk", func(context *gin.Context) {
+		path := "/";
+		diskUsage, _ := disk.DiskUsage(path);
+		fmt.Println(diskUsage);
+		context.String(200, "diskUsage");
+	})
+
+	router.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
