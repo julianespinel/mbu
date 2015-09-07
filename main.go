05c2d8d79c59712c9e9b3cd7184186913684a3c9
Julian Espinel <julianespinel@gmail.com>
2015-09-06 22:29:33 -0500

    Add stat struct. Return JSON in the /cpu route.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/disk"
+	"./resources"
 )
 
 func main() {
@@ -14,27 +15,29 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/ping", func(context *gin.Context) {
-		context.String(200, "pong");
+		context.String(200, "pong")
 	})
 
 	router.GET("/cpu", func(context *gin.Context) {
-		perCpu := true;
-		cpuPercentArray, _ := cpu.CPUPercent(time.Second, perCpu);
-		fmt.Println(cpuPercentArray);
-		context.String(200, "cpuUsage");
+		perCpu := true
+		cpuPercentArray, _ := cpu.CPUPercent(time.Second, perCpu)
+		fmt.Println(cpuPercentArray)
+		cpuUsage := resources.Stat{ "cpu", 123 }
+		fmt.Println(cpuUsage)
+		context.JSON(200, cpuUsage)
 	})
 
 	router.GET("/memory", func(context *gin.Context) {
-		vm, _ := mem.VirtualMemory();
-		fmt.Println(vm);
-		context.String(200, "memoryUsage");
+		vm, _ := mem.VirtualMemory()
+		fmt.Println(vm)
+		context.String(200, "memoryUsage")
 	})
 
 	router.GET("/disk", func(context *gin.Context) {
-		path := "/";
-		diskUsage, _ := disk.DiskUsage(path);
-		fmt.Println(diskUsage);
-		context.String(200, "diskUsage");
+		path := "/"
+		diskUsage, _ := disk.DiskUsage(path)
+		fmt.Println(diskUsage)
+		context.String(200, "diskUsage")
 	})
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
---
+++ resources/stat.go
@@ -0,0 +1,6 @@
+package resources
+
+type Stat struct {
+	Name string
+	UsagePercentage float64
+}
