6b1ce991282be24d4a112cd5a20a0ed476a834c1
Julian Espinel <julianespinel@gmail.com>
2015-09-12 14:18:02 -0500

    Define JSON data structure serialization.

--- stats/stats.go
+++ stats/stats.go
@@ -1,13 +1,19 @@
 package stats
 
 type SingleStat struct {
-	TotalGB float32
-	UsedGB float32
-	AvailableGB float32
-	UsagePercentage float32
+	TotalGB         float32 `json:"totalGB"`
+	UsedGB          float32 `json:"usedGB"`
+	AvailableGB     float32 `json:"availableGB"`
+	UsagePercentage float32 `json:"usagePercentage"`
 }
 
 type MultipleStat struct {
-	AverageUsagePercentage float32
-	UsagePercentagePerCore []float32
+	AverageUsagePercentage float32   `json:"averageUsagePercentage"`
+	UsagePercentagePerCore []float32 `json:"usagePercentagePerCore"`
+}
+
+type AllStat struct {
+	CPU  MultipleStat `json:"cpu"`
+	RAM  SingleStat   `json:"ram"`
+	Disk SingleStat   `json:"disk"`
 }
