e0d8a16ec0f50c667c0cbfe1ac81a30de2b5915a
Julian Espinel <julianespinel@gmail.com>
2015-09-12 13:31:42 -0500

    Response with real data in JSON format.

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"time"
 	"github.com/gin-gonic/gin"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/disk"
-	"./resources"
+	"./stats"
 )
+
+func getAverage(array []float32) float32 {
+
+	accumulator := float32(0)
+	size := float32(len(array))
+
+	for _, number := range array {
+		accumulator += number
+	}
+
+	return (accumulator/size)
+}
 
 func main() {
 
@@ -21,23 +32,35 @@ func main() {
 	router.GET("/cpu", func(context *gin.Context) {
 		perCpu := true
 		cpuPercentArray, _ := cpu.CPUPercent(time.Second, perCpu)
-		fmt.Println(cpuPercentArray)
-		cpuUsage := resources.Stat{ "cpu", 123 }
-		fmt.Println(cpuUsage)
-		context.JSON(200, cpuUsage)
+
+		averageUsagePercentage := getAverage(cpuPercentArray);
+		cpu := stats.MultipleStat{ averageUsagePercentage, cpuPercentArray }
+		context.JSON(200, cpu)
 	})
 
-	router.GET("/memory", func(context *gin.Context) {
+	router.GET("/ram", func(context *gin.Context) {
 		vm, _ := mem.VirtualMemory()
-		fmt.Println(vm)
-		context.String(200, "memoryUsage")
+
+		gb := float32(1000000000)
+		totalGB := float32(vm.Total) / gb
+		availableGB := float32(vm.Available) / gb
+		usedGB := (totalGB - availableGB)
+		usagePercentage := float32(vm.UsedPercent)
+		ram := stats.SingleStat{ totalGB, usedGB, availableGB, usagePercentage }
+		context.JSON(200, ram)
 	})
 
 	router.GET("/disk", func(context *gin.Context) {
 		path := "/"
 		diskUsage, _ := disk.DiskUsage(path)
-		fmt.Println(diskUsage)
-		context.String(200, "diskUsage")
+
+		gb := float32(1000000000)
+		totalGB := float32(diskUsage.Total) / gb
+		availableGB := float32(diskUsage.Free) / gb
+		usedGB := float32(diskUsage.Used) / gb
+		usagePercentage := float32(diskUsage.UsedPercent)
+		disk := stats.SingleStat{ totalGB, usedGB, availableGB, usagePercentage }
+		context.JSON(200, disk)
 	})
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
