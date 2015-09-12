ec4e33bffa916ba983121080e2b0411df0785bae
Julian Espinel <julianespinel@gmail.com>
2015-09-12 16:26:10 -0500

    Improve API routes.

--- main.go
+++ main.go
@@ -9,28 +9,32 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/ping", func(context *gin.Context) {
-		context.String(200, "pong")
-	})
+	mbu := router.Group("/mbu")
+	api := mbu.Group("/api")
+	admin := mbu.Group("/admin")
 
-	router.GET("/cpu", func(context *gin.Context) {
+	api.GET("/cpu", func(context *gin.Context) {
 		cpu := stats.GetCPUStat()
 		context.IndentedJSON(200, cpu)
 	})
 
-	router.GET("/ram", func(context *gin.Context) {
+	api.GET("/ram", func(context *gin.Context) {
 		ram := stats.GetRAMStat()
 		context.IndentedJSON(200, ram)
 	})
 
-	router.GET("/disk", func(context *gin.Context) {
+	api.GET("/disk", func(context *gin.Context) {
 		disk := stats.GetDiskStat()
 		context.IndentedJSON(200, disk)
 	})
 
-	router.GET("/all", func(context *gin.Context) {
+	api.GET("/all", func(context *gin.Context) {
 		allStats := stats.GetAllStats(stats.GetCPUStat(), stats.GetRAMStat(), stats.GetDiskStat())
 		context.IndentedJSON(200, allStats)
+	})
+
+	admin.GET("/ping", func(context *gin.Context) {
+		context.String(200, "pong")
 	})
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
