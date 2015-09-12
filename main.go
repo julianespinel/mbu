bd8b4299ae79961227de848b72dac33ad45e4b31
Julian Espinel <julianespinel@gmail.com>
2015-09-12 16:29:22 -0500

    Put API routes into blocks to improve readability.

--- main.go
+++ main.go
@@ -8,34 +8,37 @@ import (
 func main() {
 
 	router := gin.Default()
-
 	mbu := router.Group("/mbu")
+
 	api := mbu.Group("/api")
+	{
+		api.GET("/cpu", func(context *gin.Context) {
+			cpu := stats.GetCPUStat()
+			context.IndentedJSON(200, cpu)
+		})
+
+		api.GET("/ram", func(context *gin.Context) {
+			ram := stats.GetRAMStat()
+			context.IndentedJSON(200, ram)
+		})
+
+		api.GET("/disk", func(context *gin.Context) {
+			disk := stats.GetDiskStat()
+			context.IndentedJSON(200, disk)
+		})
+
+		api.GET("/all", func(context *gin.Context) {
+			allStats := stats.GetAllStats(stats.GetCPUStat(), stats.GetRAMStat(), stats.GetDiskStat())
+			context.IndentedJSON(200, allStats)
+		})
+	}
+
 	admin := mbu.Group("/admin")
-
-	api.GET("/cpu", func(context *gin.Context) {
-		cpu := stats.GetCPUStat()
-		context.IndentedJSON(200, cpu)
-	})
-
-	api.GET("/ram", func(context *gin.Context) {
-		ram := stats.GetRAMStat()
-		context.IndentedJSON(200, ram)
-	})
-
-	api.GET("/disk", func(context *gin.Context) {
-		disk := stats.GetDiskStat()
-		context.IndentedJSON(200, disk)
-	})
-
-	api.GET("/all", func(context *gin.Context) {
-		allStats := stats.GetAllStats(stats.GetCPUStat(), stats.GetRAMStat(), stats.GetDiskStat())
-		context.IndentedJSON(200, allStats)
-	})
-
-	admin.GET("/ping", func(context *gin.Context) {
-		context.String(200, "pong")
-	})
+	{
+		admin.GET("/ping", func(context *gin.Context) {
+			context.String(200, "pong")
+		})
+	}
 
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
